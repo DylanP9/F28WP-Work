8222b7c4a63e99f02581059ae35254caac019e7b
Dylan Polland <dylan.polland9@gmail.com>
2023-09-26 19:43:29 +0100

    messing about with sidebar stuff

---
+++ javaScript Exercises W2/Exercise1.js
@@ -0,0 +1,9 @@
+document.getElementById('toggleBtn').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+
+    if (sidebar.style.left === "-250px") {
+        sidebar.style.left = "0px";
+    } else {
+        sidebar.style.left = "-250px";
+    }
+});
